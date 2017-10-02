f1c7348fe6dc61f892ebb718f4a8bc63a7692f3a
Adam Holtzman <adam.holtzman@gmail.com>
2017-10-02 13:09:55 -0400

    comment

--- ReverseArrayInPlace.js
+++ ReverseArrayInPlace.js
@@ -1,8 +1,11 @@
 function reverseArrayInPlace(arr) {
+  //go over the first half of the array
   for (let i = 0; i < arr.length / 2; i++) {
+    //create temporary variable to hold element
     let tempVar = arr[i]
-
+    //set element to counterpart
     arr[i] = arr[arr.length - 1 - i]
+    //set counterpart to current element
     arr[arr.length - 1 - i] = tempVar
   }
 
