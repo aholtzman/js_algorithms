94ebb6273c9857c48560a50a0179cd39375024ee
Adam Holtzman <adam.holtzman@gmail.com>
2017-10-02 15:18:36 -0400

    Bubble Sort

---
+++ BubbleSort.js
@@ -0,0 +1,12 @@
+function bubbleSort(array) {
+  for (let i = array.length; i < 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] > arr[j + 1]) {
+        let temp = array[j]
+        array[j] = array[j + 1]
+        array[j + 1] = temp
+      }
+    }
+  }
+  return array
+}
