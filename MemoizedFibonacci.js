366cd996bc0eb38a63718c3c54d0f6bf597a119d
Adam Holtzman <adam.holtzman@gmail.com>
2017-10-02 13:34:48 -0400

    Memoized Fibonacci

---
+++ MemoizedFibonacci.js
@@ -0,0 +1,15 @@
+function fiboMemo(index, cache) {
+  //define cache
+  cache = cache || []
+  //base case
+  if (cache[index]) return cache[index]
+  else {
+    //for first two elements wich are both 1
+    if (index < 3) return 1
+    else {
+      //calculate and store numbers up to the index
+      cache[index] = fiboMemo(index -1, cache) + fiboMemo(index - 2)
+    }
+  }
+  return cache[index]
+}
